b1b0117c37eac90bb428ceee5f261d5e24942ae6
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-04 16:17:44 +0100

    CSS HTML structure - starting js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ Ad esempio:  di cosa ho bisogno per generare i numeri?
 Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Le validazioni e i controlli possiamo farli anche in un secondo momento.
  */
+
+/*
+
+*       =>Come prima cosa devo stabilire l'imput con la quale l'utente fa un select della difficoltà
+*           anche se come prima cosa sarebbe meglio puntare alle varie function
+*       => intanto cerchiamo di dare una const al parent che contiene/conterrà i quadrati
+*
+
+*/
+
+// variabile const che mi prende l'elemento del DOM
+
+const squareContainer = document.getElementById('square-container'); // dove andrò ad aggiungere gli square con un appenChild
